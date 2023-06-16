8937ffddbe90e3886057860adccd160869e99209
Deislu <129330614+Deislu@users.noreply.github.com>
2023-06-16 17:26:54 -0500

    Update base.php

--- base.php
+++ base.php
@@ -1,6 +1,31 @@
 <?php
-class conexion
-{
 
+class Conexion
+{
+    protected $db;
+    private $drive = "mysql";
+    private $host = "localhost";
+    private $namebd = "Notas2023";
+    private $user = "root";
+    private $Password = "";
+
+
+    public function __construct()
+    {
+       
+        try{
+            $db = new PDO("{$this->drive}:host={$this->host};namebd={$this->namebd}", $this->user,$this->Password);
+            
+            $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            echo "Conexion realizada";
+            echo "<br>";
+        }catch(PDOEXCEPTION $e){
+            echo "no se puede realizar la conexion ".$e.getMessage();
+            
+
+
+    }
+  }
 }
+
 ?>
