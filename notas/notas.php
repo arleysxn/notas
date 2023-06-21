4a046a4a4f160050a069e45416a7e49cac15e4a2
arley <129330609+arleysxn@users.noreply.github.com>
2023-06-21 17:34:56 -0500

    se actualizo notas.php

--- notas/notas.php
+++ notas/notas.php
@@ -1,3 +1,29 @@
-<?php
-//pagina principal ingresar al sistema 
-?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/estilos.css">
+    <title>inicio</title>
+</head>
+<body>
+<section class="estudiante-section">
+    <div class="card-grid">
+        <a class="card" href="./index.php">
+        <div class="card_background" style="background-image: url(../img/rick.webp)"></div>
+        <p class="card_category">esta opcion es unicamente para:</p>
+        <h3 class="card_heading">Ingreso</h3> 
+        </div>
+    </a>
+    <div class="card-grid">
+        <a class="card" href="">
+        <div class="card_background" style="background-image: url(../img/j.png)"></div>
+        <p class="card_category">esta opcion es unicamente para:</p>
+        <h3 class="card_heading">Registro</h3> 
+        </div>
+    </a> 
+</section>
+    
+</body>
+</html>
