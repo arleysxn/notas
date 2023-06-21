7e6c00e18e86cbefb5eea56009b030e1e3ab9c50
Deislu <129330614+Deislu@users.noreply.github.com>
2023-06-21 17:36:51 -0500

    Actualizado

---
+++ Administrador/paginas/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../../css/estilos3.css">
+    <title>Document</title>
+</head>
+<body>
+    <ul>
+        <li style="--clr:#00ade1">
+        <a href="#" data-text="&nbsp;Inicio">&nbsp;Inicio&nbsp;</a>
+      </li>
+      <li style="--clr:#ff6493">
+        <a href="#" data-text="&nbsp;Docente">&nbsp;Docente&nbsp;</a>
+      </li>
+      <li style="--clr:#ffdd1c">
+        <a href="#" data-text="&nbsp;Estudiante">&nbsp;Estudiante&nbsp;</a>
+      </li>
+      <li style="--clr:#00dc82">
+        <a href="#" data-text="&nbsp;Materias">&nbsp;Materias&nbsp;</a>
+      </li>
+      <li style="--clr:#8C009B">
+        <a href="../../index.php" data-text="&nbsp;Cerrar.sesion">&nbsp;Cerrar sesion&nbsp;</a>
+      </li>
+</ul>
+</body>
+</html>
