2c38615c1b3ad1a844c381c1a15c30b5642ced4f
Deislu <129330614+Deislu@users.noreply.github.com>
2023-06-21 17:35:57 -0500

    Actualizado

---
+++ Administrador/paginas/agregar.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="en">
+.    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE-edge">
+        <meta name= "Viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="./css/estilos.css">
+        <title>Document</title>
+        </head>
+            <body>
+                <div class="contenedor">
+                    <h2>Inicio de sesion</h2>
+                    <form action="">
+                        <div class="Usuario">
+                            <input type="text" required>
+                            <label>Correo</label>
+                        </div>
+                            <div class="Usuario">
+                            <input type="Password" required>
+                            <label>Contraseña</label>
+                        </div>
+                        <a href="">
+                            <span></span>
+                            <span></span>
+                            <span></span>
+                            <span></span>
+                            Iniciar
+        </a>
+        </from>
+        </div>
+        
+    </body>
+        </html><!DOCTYPE html>
+<html lang="en">
+.    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE-edge">
+        <meta name= "Viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="./css/estilos.css">
+        <title>Document</title>
+        </head>
+            <body>
+                <div class="contenedor">
+                    <h2>Inicio de sesion</h2>
+                    <form action="">
+                        <div class="Usuario">
+                            <input type="text" required>
+                            <label>Correo</label>
+                        </div>
+                            <div class="Usuario">
+                            <input type="Password" required>
+                            <label>Contraseña</label>
+                        </div>
+                        <a href="">
+                            <span></span>
+                            <span></span>
+                            <span></span>
+                            <span></span>
+                            Iniciar
+        </a>
+        </from>
+        </div>
+        
+    </body>
+        </html>
