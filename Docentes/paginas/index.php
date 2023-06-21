0f9f7728196f90e8406a73a8f63a39833a14d4e7
Deislu <129330614+Deislu@users.noreply.github.com>
2023-06-21 17:40:24 -0500

    Actualizado

---
+++ Docentes/paginas/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../../css/estilos3.css">
+    <title>Document</title>
+</head>
+<body>
+    <ul>
+        <li style="--clr:#ffdd1c">
+        <a href="#" data-text="&nbsp;Materias">&nbsp;Materias&nbsp;</a>
+      </li>
+      <li style="--clr:#00dc82">
+        <a href="#" data-text="&nbsp;Agregar.notas">&nbsp;Agregar notas&nbsp;</a>
+      </li>
+</ul>
+</body>
+</html>
