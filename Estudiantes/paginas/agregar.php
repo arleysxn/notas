fd8302645f7f7932a9412535e61dc27aacf5ff38
Deislu <129330614+Deislu@users.noreply.github.com>
2023-06-22 17:27:16 -0500

    formulario

---
+++ Estudiantes/paginas/agregar.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE-edge">
+        <meta name= "Viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="../../css/estiloEs.css">
+        <title>Document</title>
+        </head>
+            <body>
+                <div class="Contenedor">
+                    <h2>Formulario</h2>
+                    <form action="">
+                        <div class="Usuario">
+                            <input type="text" required>
+                            <label>Nombre</label>
+                        </div>
+                            <div class="Usuario">
+                            <input type="text" required>
+                            <label>Apellido</label>
+                        </div>
+                            <div class="Usuario">
+                            <input type="number" required>
+                            <label>Documento</label>
+                        </div>
+                            <div class="Usuario">
+                            <input type="text" required>
+                            <label>Correo</label>
+                        </div>                                  
+                            <div class="Usuario">
+                            <input type="text" required>
+                            <label>Materia</label>
+                        </div>
+                            <div class="Usuario">
+                            <input type="text" required>
+                            <label>Docente</label>
+                        </div>   
+                            <div class="Usuario">
+                            <input type="text" required>
+                            <label>Promedio</label>
+                        </div>
+                            <div class="Usuario">
+                            <input type="number" required>
+                            <label>Fecha registro</label>
+                        </div>      
+                               
+                        <a href="">
+                            <span></span>
+                            <span></span>
+                            <span></span>
+                            <span></span>
+                            Enviar
+        </a>
+        </from>
+        </div>
+        
+    </body>
+</html>
+        </html>
