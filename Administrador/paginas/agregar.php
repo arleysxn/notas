6ff924a7b5652ec71f7e75eef69687cf95145c5f
Deislu <129330614+Deislu@users.noreply.github.com>
2023-06-22 17:19:51 -0500

    Registro

--- Administrador/paginas/agregar.php
+++ Administrador/paginas/agregar.php
@@ -1,35 +1,59 @@
 <!DOCTYPE html>
 <html lang="en">
-.    <head>
+    <head>
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE-edge">
         <meta name= "Viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="../../css/estilos.css">
+        <link rel="stylesheet" href="../../css/estilosIN.CSS">
         <title>Document</title>
         </head>
             <body>
                 <div class="Contenedor">
-                    <h2>Inicio de sesion</h2>
+                    <h2>Registro</h2>
                     <form action="">
                         <div class="Usuario">
                             <input type="text" required>
-                            <label>Correo</label>
+                            <label>Nombre</label>
                         </div>
                             <div class="Usuario">
-                            <input type="Password" required>
-                            <label>Contraseña</label>
+                            <input type="text" required>
+                            <label>Apellido</label>
                             </div>
-                            <div>
-                            <p href="index.php">olvidaste tu contraseña?</p>
+                            <div class="Usuario">
+                            <input type="text" required>
+                            <label>Usuario</label>
+                        </div>
+                            <div class="Usuario">
+                            <input type="text" required>
+                            <label>Contraseña</label>
+                        </div>
+                            <div class="form-group">
+                                <p>Perfil:
+                                    <label for="perfil"></label>
+                                    <select name="txtperfil">
+                                        <option></option>
+                                        <option value="Administrador">Administrador</option>
+                                        <option value="Docente">Docentes</option>
+                                    </select>
+                                </p>
                            </div>
-                            
+                           <div class="form-group">
+                            <p>Estado:
+                                <label for="perfil"></label>
+                                <select name="txtestado">
+                                    <option></option>
+                                    <option value="Activo">Activo</option>
+                                    <option value="No activo">No activo</option>
+                                </select>
+                                </p> 
+                            </div>                                   
                             
                         <a href="">
                             <span></span>
                             <span></span>
                             <span></span>
                             <span></span>
-                            Iniciar
+                            Registrarme
         </a>
         </from>
         </div>
