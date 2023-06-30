1ddb905e4499106d23c89fc11fec18ef4e990678
arley <129330609+arleysxn@users.noreply.github.com>
2023-06-30 00:04:43 -0500

    Actualizado

--- notas-Deisy/Administrador/paginas/eliminar.php
+++ notas-Deisy/Administrador/paginas/eliminar.php
@@ -4,7 +4,7 @@
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE-edge">
         <meta name= "Viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="../../css/estilosR.css">
+        <link rel="stylesheet" href="../../css/estilosEL.css">
         <title>Document</title>
         </head>
             <body>
@@ -13,7 +13,7 @@
                     <form action="../controladores/eliminarusuario.php" method="POST">
                         <div class="Usuario">
                             <input type="text" name="Id">
-                            <label>ID</label>
+                            <h1>ID</h1>
                         </div>  
 
                         <button type="submit" class="btn btn-danger">ELIMINAR</button>
